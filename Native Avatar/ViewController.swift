2a2ad3d31bd9a5f9b492cc5135c810b7f44b6a1e
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-22 17:32:59 +0300

    Create project

---
+++ Native Avatar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Native Avatar
+//
+//  Created by Sergei Semko on 5/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
