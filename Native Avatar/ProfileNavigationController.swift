2e0dcc35385eb99a5b339947fb646297bd264d80
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-25 13:37:40 +0300

    Best solution

---
+++ Native Avatar/ProfileNavigationController.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileNavigationController.swift
+//  Native Avatar
+//
+//  Created by Sergei Semko on 5/22/23.
+//
+
+import UIKit
+
+open class ProfileNavigationController: UINavigationController, UINavigationControllerDelegate {
+    
+    let avatarImage = UIImageView()
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+        
+        avatarImage.image = .init(systemName: "person.crop.circle.fill")
+        avatarImage.contentMode = .scaleAspectFit
+        avatarImage.tintColor = .systemGray3
+
+        delegate = self
+    }
+
+    public func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        
+        let navBar = navigationBar
+        navBar.subviews.forEach { subview in
+            let stringFromClass = NSStringFromClass(subview.classForCoder)
+            guard stringFromClass.contains("UINavigationBarLargeTitleView") else { return }
+            
+            if avatarImage.superview != subview {
+                avatarImage.removeFromSuperview()
+                subview.addSubview(avatarImage)
+            }
+            
+            avatarImage.translatesAutoresizingMaskIntoConstraints = false
+            avatarImage.removeConstraints(avatarImage.constraints)
+            
+            avatarImage.widthAnchor.constraint(equalToConstant: 36).isActive = true
+            avatarImage.heightAnchor.constraint(equalToConstant: 36).isActive = true
+            avatarImage.bottomAnchor.constraint(equalTo: avatarImage.superview!.bottomAnchor, constant: -10).isActive = true
+            avatarImage.trailingAnchor.constraint(equalTo: subview.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+            
+        }
+    }
+}
+
---
+++ Native Avatar/ScrollController.swift
@@ -0,0 +1,31 @@
+//
+//  ScrollController.swift
+//  Native Avatar
+//
+//  Created by Sergei Semko on 5/24/23.
+//
+
+import UIKit
+
+class ScrollController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Avatar"
+        
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.contentSize = .init(width: view.frame.width, height: 2000)
+
+        view.addSubview(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+    }
+    
+}
